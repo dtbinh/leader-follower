f7d02732c1b0c44b321284f54e3318c1c7f1b3c8
olsoncc <chris.olson@jhuapl.edu>
2016-05-05 19:06:22 -0400

    Implemented initial search behavior. All it does is spin around in a circle.

--- leader_follower/lfSearchBehavior.c
+++ leader_follower/lfSearchBehavior.c
@@ -1,12 +1,19 @@
-#include "lfSearchBehavior.h"
-
 #include "inc/hw_types.h"
 #include "lfMotors.h"
+#include "lfSensors.h"
 #include "lfUtility.h"
 
+#include "lfSearchBehavior.h"
 
-void search(void)
+void search(DisplayArgs * const args)
 {
+   IrDistance leftDist = args->distanceL;
+   IrDistance rightDist = args->distanceR;
 
+   if(leftDist < 0 || rightDist < 0)
+   {
+      return;
+   }
 
+   turn(15);
 }
--- leader_follower/lfSearchBehavior.h
+++ leader_follower/lfSearchBehavior.h
@@ -1,8 +1,10 @@
 #ifndef LFSEARCHBEHAVIOR_H_
 #define LFSEARCHBEHAVIOR_H_
 
+#include "lfDisplay.h"
+
 // The follower robot has lost track of the leader robot.
 // The follower must now search for the leader robot.
-void search(void);
+void search(DisplayArgs * const args);
 
 #endif /* LFSEARCHBEHAVIOR_H_ */
